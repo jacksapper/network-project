6b114793af9ecce4309ab9700013154f7c4aa1ae
Jason Hatton <jason.a.hatton@gmail.com>
2015-11-27 19:54:22 -0600

    Added simple shell script to fufill the reading from file requirement.  This floods the server with requests to show that the server is operating concurrently.

---
+++ auto.sh
@@ -0,0 +1,80 @@
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+./Client 127.0.0.1 5193 Hello
+
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+./Client 127.0.0.1 5193 asdffdsa
+
